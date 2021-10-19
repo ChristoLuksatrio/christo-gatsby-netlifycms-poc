892e9db71c097702922a0f8855293f4870b18f8e
Christo <christoluksatrio@Christos-MacBook-Air.local>
2021-10-19 14:55:57 -0400

    added random page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -78,6 +78,9 @@ const Navbar = class extends React.Component {
               <Link className="navbar-item" to="/contact/examples">
                 Form Examples
               </Link>
+              <Link className="navbar-item" to="/random">
+                Random
+              </Link>
             </div>
             <div className="navbar-end has-text-centered">
               <a
---
+++ src/templates/random-page.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import Layout from '../components/Layout'
+
+export const Random = ({ title, content }) => {
+
+  return (
+      <Layout>
+        <section className="section section--gradient">
+        <div className="container">
+            <div className="columns">
+            <div className="column is-10 is-offset-1">
+                <div className="section">
+                <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
+                    Hello World
+                </h2>
+                </div>
+            </div>
+            </div>
+        </div>
+        </section>
+      </Layout>
+  )
+}
+
+
+export default Random
